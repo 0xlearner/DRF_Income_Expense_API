66ab0fd396d4ef6ad79956b30d7bfc7b00fc1a14
zeroxlearner <zeroxlearner@gmail.com>
2021-12-17 13:42:06 +0500

    Sending-Account-Activation-Email

--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
 from rest_framework import generics, status
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 from .serializers import RegisterSerializer
+from .models import User
+from .utils import Util
 
 
 class RegisterView(generics.GenericAPIView):
@@ -16,4 +21,30 @@ class RegisterView(generics.GenericAPIView):
 
         user_data = serializer.data
 
+        user = User.objects.get(email=user_data["email"])
+
+        token = RefreshToken.for_user(user).access_token
+
+        current_site = get_current_site(request).domain
+        relative_link = reverse("email-verify")
+        absolute_url = "http://" + current_site + relative_link + "?token=" + str(token)
+        email_body = (
+            "Hi"
+            + user.username
+            + "Use link below to verify your email\n"
+            + absolute_url
+        )
+        data = {
+            "email_body": email_body,
+            "to_email": user.email,
+            "email_subject": "Verify your Email",
+        }
+
+        Util.send_email(data)
+
         return Response(user_data, status=status.HTTP_201_CREATED)
+
+
+class VerifyEmail(generics.GenericAPIView):
+    def get(self):
+        pass
--- income_expenses_api/settings.py
+++ income_expenses_api/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -84,7 +84,12 @@ DATABASES = {
     }
 }
 
-REST_FRAMEWORK = {"NON_FIELD_ERRORS_KEY": "error"}
+REST_FRAMEWORK = {
+    "NON_FIELD_ERRORS_KEY": "error",
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
 
 
 # Password validation
@@ -127,3 +132,11 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_USE_TLS = True
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+print(EMAIL_HOST_USER)
